f02cc49b339ff9962d32c5937d288a55e4188f97
Mohammad Javad Fadaeieslam <mj.fadaei@gmail.com>
2021-12-04 14:05:05 +0330

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,21 @@
+#2
+#bar hasb radian
+
+import math
+n=int(input("Pleas Enter a Number in degrees: "))
+
+print("sin=%.2f" %math.sin(math.radians(n)))
+
+print("cos=%.2f" %math.cos(math.radians(n)))
+
+print("tan=%.2f" %math.tan(math.radians(n)))
+
+
+
+m=float(input("Pleas Enter a Number in radians: "))
+
+print("sin=%.2f" %math.sin(m))
+
+print("cos=%.2f" %math.cos(m))
+
+print("tan=%.2f" %math.tan(m))
